8587eefaee6f2f6dec3ff5c0d245a8332fa806dd
Rushikesh <rushikeshudhoji880@gmail.com>
2026-05-17 18:08:22 +0530

    test automation with jenkins

--- src/main/java/com/weather/service/WeatherService.java
+++ src/main/java/com/weather/service/WeatherService.java
@@ -5,6 +5,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
+
     private final RestTemplate restTemplate;
 
     public WeatherService(RestTemplate restTemplate) {
