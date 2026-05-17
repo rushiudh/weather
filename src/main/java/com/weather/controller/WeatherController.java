fc0e2a526e20836e424c09d490a1cd359cd2ead2
Rushikesh <rushikeshudhoji880@gmail.com>
2026-05-17 17:59:11 +0530

    optimized imports

--- src/main/java/com/weather/controller/WeatherController.java
+++ src/main/java/com/weather/controller/WeatherController.java
@@ -4,7 +4,6 @@ import com.weather.dto.Coordinates;
 import com.weather.service.WeatherService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/weather/service/WeatherService.java
+++ src/main/java/com/weather/service/WeatherService.java
@@ -1,6 +1,5 @@
 package com.weather.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
