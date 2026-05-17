58db8511a8c0dd0cf317def5f71609b307933449
Rushikesh <rushikeshudhoji880@gmail.com>
2026-05-17 19:17:22 +0530

    communication in frontend and backend

--- src/main/java/com/weather/controller/WeatherController.java
+++ src/main/java/com/weather/controller/WeatherController.java
@@ -2,7 +2,7 @@ package com.weather.controller;
 
 import com.weather.dto.Coordinates;
 import com.weather.service.WeatherService;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +14,7 @@ public class WeatherController {
         this.weatherService = weatherService;
     }
 
-    @GetMapping("/weather")
+    @PostMapping("/weather")
     public String getWeather(@RequestBody Coordinates coordinates) {
         return weatherService.getWeather(coordinates.latitude(), coordinates.longitude());
     }
